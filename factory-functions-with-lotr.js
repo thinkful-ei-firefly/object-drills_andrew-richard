b0b7558102eee516156a7c8ad796c053507da68b
rjgates3 <gates.j.richard@gmail.com>
2019-07-10 14:44:58 -0600

    updated

--- factory-functions-with-lotr.js
+++ factory-functions-with-lotr.js
@@ -1,29 +1,37 @@
 'use strict';
 
-function createCharacter(name, nickname, race, origin, attack, defense) {
-     return {
-        name,
-        nickname,
-        race,
-        origin,
-        attack,
-        defense,
-        describe: function(){console.log(`${name} is a ${race} from ${origin}.`)},
-        evaluateFight: function(character){
-            let damageTaken = character.attack - this.defense;
-            let damageGiven = this.attack - character.defense;
-            if (damageTaken < 0){
-                damageTaken = 0;
-            }
-            if (damageGiven < 0){
-                damageGiven = 0;
-            }
-            console.log(`Your opponent takes ${damageGiven} damage and you receive ${damageTaken} damage.`);
-         }
+function createCharacter(name, nickname, race, origin, attack, defense, weapon = 'none') {
+  return {
+    name,
+    nickname,
+    race,
+    origin,
+    attack,
+    defense,
+    weapon,
+    describe: function(){
+      if (this.weapon === 'none') {
+        console.log(`${name} is a ${race} from ${origin}.`);
+      }
+      else {
+        console.log(`${name} of the ${origin} who uses a ${weapon}`);
+      }
+    },
+    evaluateFight: function(character){
+      let damageTaken = character.attack - this.defense;
+      let damageGiven = this.attack - character.defense;
+      if (damageTaken < 0){
+        damageTaken = 0;
+      }
+      if (damageGiven < 0){
+        damageGiven = 0;
+      }
+      console.log(`Your opponent takes ${damageGiven} damage and you receive ${damageTaken} damage.`);
     }
+  }
 }
 
-let gandalf = createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6);
+let gandalf = createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6, 'staff');
 let bilbo = createCharacter('Bilbo Baggins', 'bilbo', 'Hobbit', 'The Shire', 2, 1);
 let frodo = createCharacter('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2);
 let aragorn = createCharacter('Aragorn son of Arathorn', 'aragorn', 'Man', 'Dunnedain', 6, 6);
@@ -38,7 +46,19 @@ gandalf.evaluateFight(bilbo);
 
 characters.push(arwen);
 
-console.log(characters);
+//console.log(characters);
 
 characters.find(function(char){return char === aragorn}).describe();
 
+let isHobbit = characters.filter(function(char){
+  return char.race === 'Hobbit';
+});
+
+let attackAbove5 = characters.filter(function(char){
+  return char.attack > 5;
+});
+
+
+gandalf.weapon = 'wizard staff';
+
+gandalf.describe();
